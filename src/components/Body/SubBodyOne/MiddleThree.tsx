9c4383e3003af65d85786868ecdff5d6f18b66d1
munezeromicha <munezerontaganiramichel@gmail.com>
2024-09-04 09:29:06 +0200

    fix-3D-animation-card-color

--- src/components/Body/SubBodyOne/MiddleThree.tsx
+++ src/components/Body/SubBodyOne/MiddleThree.tsx
@@ -13,12 +13,12 @@ const MiddleThree: React.FC = () => {
         <div className="relative">
           <div className="flex animate-marquee space-x-4">
             {/* Card 1 */}
-            <div className="bg-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
-            <div className="text-4xl text-blue-900 mb-4">“</div>
-            <p className="text-gray-700 mb-8">
+            <div className="bg-blue-500 text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
+            <div className="text-4xl mb-4">“</div>
+            <p className="mb-8">
               Whitepace is designed as a collaboration tool for businesses that is a full project management solution.
             </p>
-            <hr className="mb-4" />
+            <hr className="border-white mb-4" />
             <div className="flex items-center">
               <img
                 src={Avatar}
@@ -26,8 +26,8 @@ const MiddleThree: React.FC = () => {
                 className="w-12 h-12 rounded-full mr-4"
               />
               <div>
-                <p className="font-bold text-gray-900">Oberon Shaw, MCH</p>
-                <p className="text-sm text-gray-600">Head of Talent Acquisition, North America</p>
+                <p className="font-bold">Oberon Shaw, MCH</p>
+                <p className="text-sm">Head of Talent Acquisition, North America</p>
               </div>
             </div>
             </div>
