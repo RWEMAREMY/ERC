3719ff62867e0d1763caa8fbcfd9113cb026afe9
RWEMAREMY <rwemaremy21@gmail.com>
2024-10-19 17:23:25 +0200

    new button to publication added in middle three

--- src/components/Body/MiddleThree.tsx
+++ src/components/Body/MiddleThree.tsx
@@ -1,330 +1,235 @@
-import React, { useState } from 'react';
-import Avatar from '../../assets/Images/Avater.png'
-import { useEffect, useRef } from 'react';
-
-import { Link, useLocation } from 'react-router-dom';
-
+import React, { useState, useEffect, useRef } from "react";
+import Avatar from "../../assets/Images/Avater.png";
+import { useLocation, Link } from "react-router-dom";
 
 const MiddleThree: React.FC = () => {
-
   const location = useLocation();
-
   const publicationsRef = useRef<HTMLDivElement>(null);
-
   const scrollContainerRef = useRef<HTMLDivElement>(null);
-
   const [scrollDirection, setScrollDirection] = useState(1);
 
-  // const [isCursorOver, setIsCursorOver] = useState(false); // New state to track cursor position
-
-
   useEffect(() => {
-
-    if(location.state && location.state.scrollToPublications){
-
-      publicationsRef.current?.scrollIntoView({behavior: 'smooth'})
-
+    if (location.state && location.state.scrollToPublications) {
+      publicationsRef.current?.scrollIntoView({ behavior: "smooth" });
     }
-
-  },[location]);
-
+  }, [location]);
 
   useEffect(() => {
-
     const scrollContainer = scrollContainerRef.current;
-
     if (!scrollContainer) return;
 
-
     let scrolling = true;
-
     let animationId: number;
-
     let startX: number;
     let scrollLeft: number;
 
-
     const scroll = () => {
-
       if (scrolling && scrollContainer) {
-
         scrollContainer.scrollLeft += scrollDirection;
 
-        
-
-        // Check if we've reached the end or beginning
-
-        if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
-
-          setScrollDirection(-1); // Start scrolling left
-
+        if (
+          scrollContainer.scrollLeft >=
+          scrollContainer.scrollWidth - scrollContainer.clientWidth
+        ) {
+          setScrollDirection(-1);
         } else if (scrollContainer.scrollLeft <= 0) {
-
-          setScrollDirection(1); // Start scrolling right
+          setScrollDirection(1);
         }
       }
       animationId = requestAnimationFrame(scroll);
     };
 
-    const handleMouseEnter = () => {
-      scrolling = true; // Start scrolling when mouse enters
-    };
-
-    const handleMouseLeave = () => {
-      scrolling = false; // Stop scrolling when mouse leaves
-    };
-
     const handleMouseDown = (e: MouseEvent) => {
-
       scrolling = false;
       startX = e.pageX - scrollContainer.offsetLeft;
       scrollLeft = scrollContainer.scrollLeft;
-
     };
 
     const handleMouseUp = () => {
-
       scrolling = true;
-
     };
 
     const handleMouseMove = (e: MouseEvent) => {
-
       if (!scrolling) {
         const x = e.pageX - scrollContainer.offsetLeft;
-
         const walk = (x - startX) * 2;
-
         scrollContainer.scrollLeft = scrollLeft - walk;
-
       }
-
     };
 
+    const handleTouchStart = (e: TouchEvent) => {
+      scrolling = false;
+      startX = e.touches[0].clientX - scrollContainer.offsetLeft;
+      scrollLeft = scrollContainer.scrollLeft;
+    };
 
-    scrollContainer.addEventListener('mouseenter', handleMouseEnter); // Add mouse enter event
+    const handleTouchMove = (e: TouchEvent) => {
+      if (!scrolling) {
+        const x = e.touches[0].clientX - scrollContainer.offsetLeft;
+        const walk = (x - startX) * 2;
+        scrollContainer.scrollLeft = scrollLeft - walk;
+      }
+    };
 
-    scrollContainer.addEventListener('mouseleave', handleMouseLeave); // Add mouse leave event
+    scrollContainer.addEventListener("mousedown", handleMouseDown);
+    scrollContainer.addEventListener("mouseup", handleMouseUp);
+    scrollContainer.addEventListener("mousemove", handleMouseMove);
+    scrollContainer.addEventListener("mouseleave", handleMouseUp);
 
-    scrollContainer.addEventListener('mousedown', handleMouseDown);
-
-    scrollContainer.addEventListener('mouseup', handleMouseUp);
-
-    scrollContainer.addEventListener('mousemove', handleMouseMove);
-
+    scrollContainer.addEventListener("touchstart", handleTouchStart);
+    scrollContainer.addEventListener("touchmove", handleTouchMove);
 
     animationId = requestAnimationFrame(scroll);
 
-
     return () => {
-
       cancelAnimationFrame(animationId);
-
-      scrollContainer.removeEventListener('mouseenter', handleMouseEnter); // Clean up
-
-      scrollContainer.removeEventListener('mouseleave', handleMouseLeave); // Clean up
-
-      scrollContainer.removeEventListener('mousedown', handleMouseDown);
-
-      scrollContainer.removeEventListener('mouseup', handleMouseUp);
-
-      scrollContainer.removeEventListener('mousemove', handleMouseMove);
-
+      scrollContainer.removeEventListener("mousedown", handleMouseDown);
+      scrollContainer.removeEventListener("mouseup", handleMouseUp);
+      scrollContainer.removeEventListener("mousemove", handleMouseMove);
+      scrollContainer.removeEventListener("mouseleave", handleMouseUp);
+      scrollContainer.removeEventListener("touchstart", handleTouchStart);
+      scrollContainer.removeEventListener("touchmove", handleTouchMove);
     };
-
   }, [scrollDirection]);
 
   return (
-
     <section ref={publicationsRef} className="py-16 bg-gray-50 overflow-hidden">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold text-gray-900 mb-4">Publications</h2>
         <p className="text-gray-600 mb-12">
-
-          We provide both technical assistance and statistical consultancy to our clients.
+          We provide both technical assistance and statistical consultancy to
+          our clients.
         </p>
 
-
         <div className="relative">
-
-        <div 
-
+          <div
             ref={scrollContainerRef}
-            className="flex space-x-4 overflow-x-hidden cursor-grab active:cursor-grabbing"
-
-            style={{ userSelect: 'none' }}
+            className="flex space-x-4 overflow-x-hidden cursor-grab active:cursor-grabbing p-2"
+            style={{ userSelect: "none" }}
           >
-
             {/* Card 1 */}
-            <div className="bg-blue-500 text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
-
-            <div className="text-4xl mb-4">“</div>
-            <p className="mb-8">
-              Whitepace is designed as a collaboration tool for businesses that is a full project management solution.
-            </p>
-
-            <hr className="border-white mb-4" />
-            <div className="flex items-center">
-
-              <img
-
-                src={Avatar}
-                alt="Profile"
-                className="w-12 h-12 rounded-full mr-4"
-
-              />
-
-              <div>
-                <p className="font-bold">Oberon Shaw, MCH</p>
-                <p className="text-sm">Head of Talent Acquisition, North America</p>
+            <div className="bg-[#043873] text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
+              <div className="text-4xl mb-4">“</div>
+              <p className="mb-8">
+                Whitepace is designed as a collaboration tool for businesses
+                that is a full project management solution.
+              </p>
+              <hr className="border-white mb-4" />
+              <div className="flex items-center">
+                <img
+                  src={Avatar}
+                  alt="Profile"
+                  className="w-12 h-12 rounded-full mr-4"
+                />
+                <div>
+                  <p className="font-bold">Oberon Shaw, MCH</p>
+                  <p className="text-sm">
+                    Head of Talent Acquisition, North America
+                  </p>
+                </div>
               </div>
-            </div>
             </div>
 
             {/* Card 2 */}
-            <div className="bg-blue-500 text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
-
-            <div className="text-4xl mb-4">“</div>
-
-            <p className="mb-8">
-              Whitepace is designed as a collaboration tool for businesses that is a full project management solution.
-
-            </p>
-            <hr className="border-white mb-4" />
-            <div className="flex items-center">
-              <img
-
-                src={Avatar}
-                alt="Profile"
-
-                className="w-12 h-12 rounded-full mr-4"
-
-              />
-              <div>
-
-                <p className="font-bold">Oberon Shaw, MCH</p>
-
-                <p className="text-sm">Head of Talent Acquisition, North America</p>
-
-              </div>
-
-            </div>
-            </div>
-
-
-            <div className="bg-blue-500 text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
-
-            <div className="text-4xl mb-4">“</div>
-
-            <p className="mb-8">
-
-              Whitepace is designed as a collaboration tool for businesses that is a full project management solution.
-            </p>
-
-            <hr className="border-white mb-4" />
-            <div className="flex items-center">
-              <img
-
-                src={Avatar}
-                alt="Profile"
-
-                className="w-12 h-12 rounded-full mr-4"
-              />
-
-              <div>
-                <p className="font-bold">Oberon Shaw, MCH</p>
-
-                <p className="text-sm">Head of Talent Acquisition, North America</p>
+            <div className="bg-[#043873] text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
+              <div className="text-4xl mb-4">“</div>
+              <p className="mb-8">
+                Whitepace is designed as a collaboration tool for businesses
+                that is a full project management solution.
+              </p>
+              <hr className="border-white mb-4" />
+              <div className="flex items-center">
+                <img
+                  src={Avatar}
+                  alt="Profile"
+                  className="w-12 h-12 rounded-full mr-4"
+                />
+                <div>
+                  <p className="font-bold">Oberon Shaw, MCH</p>
+                  <p className="text-sm">
+                    Head of Talent Acquisition, North America
+                  </p>
+                </div>
               </div>
             </div>
-            </div>
 
-            <div className="bg-blue-500 text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
-            <div className="text-4xl mb-4">“</div>
-
-            <p className="mb-8">
-              Whitepace is designed as a collaboration tool for businesses that is a full project management solution.
-
-            </p>
-
-            <hr className="border-white mb-4" />
-
-            <div className="flex items-center">
-
-              <img
-
-                src={Avatar}
-
-                alt="Profile"
-                className="w-12 h-12 rounded-full mr-4"
-
-              />
-
-              <div>
-
-                <p className="font-bold">Oberon Shaw, MCH</p>
-
-                <p className="text-sm">Head of Talent Acquisition, North America</p>
+            <div className="bg-[#043873] text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
+              <div className="text-4xl mb-4">“</div>
+              <p className="mb-8">
+                Whitepace is designed as a collaboration tool for businesses
+                that is a full project management solution.
+              </p>
+              <hr className="border-white mb-4" />
+              <div className="flex items-center">
+                <img
+                  src={Avatar}
+                  alt="Profile"
+                  className="w-12 h-12 rounded-full mr-4"
+                />
+                <div>
+                  <p className="font-bold">Oberon Shaw, MCH</p>
+                  <p className="text-sm">
+                    Head of Talent Acquisition, North America
+                  </p>
+                </div>
               </div>
             </div>
-            </div>
 
-
-            <div className="bg-blue-500 text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
-
-            <div className="text-4xl mb-4">“</div>
-
-            <p className="mb-8">
-              Whitepace is designed as a collaboration tool for businesses that is a full project management solution.
-            </p>
-
-            <hr className="border-white mb-4" />
-            <div className="flex items-center">
-
-              <img
-                src={Avatar}
-
-                alt="Profile"
-
-                className="w-12 h-12 rounded-full mr-4"
-              />
-              <div>
-
-                <p className="font-bold">Oberon Shaw, MCH</p>
-                <p className="text-sm">Head of Talent Acquisition, North America</p>
-
+            <div className="bg-[#043873] text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
+              <div className="text-4xl mb-4">“</div>
+              <p className="mb-8">
+                Whitepace is designed as a collaboration tool for businesses
+                that is a full project management solution.
+              </p>
+              <hr className="border-white mb-4" />
+              <div className="flex items-center">
+                <img
+                  src={Avatar}
+                  alt="Profile"
+                  className="w-12 h-12 rounded-full mr-4"
+                />
+                <div>
+                  <p className="font-bold">Oberon Shaw, MCH</p>
+                  <p className="text-sm">
+                    Head of Talent Acquisition, North America
+                  </p>
+                </div>
               </div>
             </div>
+
+            <div className="bg-[#043873] text-white shadow-lg rounded-lg p-6 text-left w-80 flex-shrink-0">
+              <div className="text-4xl mb-4">“</div>
+              <p className="mb-8">
+                Whitepace is designed as a collaboration tool for businesses
+                that is a full project management solution.
+              </p>
+              <hr className="border-white mb-4" />
+              <div className="flex items-center">
+                <img
+                  src={Avatar}
+                  alt="Profile"
+                  className="w-12 h-12 rounded-full mr-4"
+                />
+                <div>
+                  <p className="font-bold">Oberon Shaw, MCH</p>
+                  <p className="text-sm">
+                    Head of Talent Acquisition, North America
+                  </p>
+                </div>
+              </div>
             </div>
-
-
-            {/* Card 3, 4, 5 */}
-
-            {/* ... (repeat the same structure for other cards) ... */}
-
           </div>
-
-          <div>
-
-          <button className='bg-blue-500 rounded-xl p-2 mt-2 font-back hover:bg-orange-300 hover:font-bold '>
-
-            <Link to="/publications" className="text-white hover:text-black ">
-
-                  more publications <i className="fa-solid fa-arrow-right"></i>
-
-                </Link>
-
+          <div className="p-4">
+            <button className="bg-[#043873] rounded-xl p-4 mt-2 font-back hover:bg-[#df4e10] hover:font-bold ">
+              <Link to="/publications" className="text-white hover:text-white">
+                More publications <i className="fa-solid fa-arrow-right"></i>
+              </Link>
             </button>
           </div>
         </div>
-
-        {/* Remove the dots */}
       </div>
-
     </section>
   );
-
 };
-
 
 export default MiddleThree;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import Logo from "../../assets/Images/ERC Logo 2.png";
 import Layout from "../../pages/Layout";
 import { useNavigate, NavLink } from "react-router-dom";
 import globe from "../../assets/GIF/globeGIF.gif";
+import BookmarkButton from "../Body/BookmarkButton";
 
 const useTypingEffect = (text: string, typingSpeed: number = 50) => {
   const [displayedText, setDisplayedText] = useState("");
@@ -42,7 +43,7 @@ const Header: React.FC = () => {
 
   const paragraphText =
     "ERC's mission is to work in the direction of accelerating the time to value and maximize the investment of our clients around the world.";
-  const displayedText = useTypingEffect(paragraphText, 30);
+  const displayedText = useTypingEffect(paragraphText, 200);
 
   useEffect(() => {
     setAnimate(true);
@@ -140,9 +141,7 @@ const Header: React.FC = () => {
           />
         </div>
 
-        <nav
-          className={`flex items-center hidden md:block`}
-        >
+        <nav className={`flex items-center hidden md:block`}>
           <ul className="flex space-x-6">
             {navItems.map((item) => (
               <li key={item.to} className="relative">
@@ -187,6 +186,34 @@ const Header: React.FC = () => {
             ))}
           </ul>
         </nav>
+
+        {/* Mobile Dropdown Menu */}
+        <div
+          className={`md:hidden ${servicesDropdownOpen ? "block" : "hidden"}`}
+        >
+          <button
+            onClick={() => setServicesDropdownOpen(!servicesDropdownOpen)}
+            className="text-white hover:text-[#DF4E10]"
+          >
+            Services
+          </button>
+          {servicesDropdownOpen && (
+            <ul className="flex flex-col bg-[#043873] rounded-md shadow-xl mt-2">
+              {navItems
+                .find((item) => item.label === "Services")
+                ?.subItems?.map((subItem) => (
+                  <li key={subItem.to}>
+                    <NavLink
+                      to={subItem.to}
+                      className="block px-4 py-2 text-sm text-white hover:bg-[#0A4D8F] hover:text-[#DF4E10]"
+                    >
+                      {subItem.label}
+                    </NavLink>
+                  </li>
+                ))}
+            </ul>
+          )}
+        </div>
 
         <div className="flex items-center space-x-4">
           <div className="relative pl-8">
@@ -251,17 +278,7 @@ const Header: React.FC = () => {
       >
         <Layout />
       </div>
-<<<<<<< HEAD
-      <div className="flex justify-between">
-        <div className="flex flex-col justify-center items-start px-4 sm:px-8 space-y-4 sm:space-y-6 mt-8 sm:mt-20 relative z-10">
-          <h1 className="text-4xl sm:text-6xl md:text-8xl font-bold transition-all duration-1000">
-            Econometer
-          </h1>
-          <h2 className="text-3xl sm:text-4xl md:text-6xl font-bold transition-all duration-1000 border-b-4 rounded-b-sm">
-            Research Center
-          </h2>
-          <p className="text-base sm:text-lg text-[#DF4E10] font-bold max-w-xl h-20 typing-cursor">
-=======
+
       <div className="flex justify-between flex-col md:flex-row">
         <div className="flex flex-col justify-center items-start px-4 sm:px-8 space-y-4 sm:space-y-6 mt-8 sm:mt-20 relative z-10">
           <h1 className="text-4xl sm:text-3xl md:text-5xl lg:text-[3.5rem] font-bold transition-all duration-1000">
@@ -271,38 +288,24 @@ const Header: React.FC = () => {
             Research Center
           </h2>
           <p className="text-sm sm:text-base lg:text-lg text-[#DF4E10] font-bold max-w-xl h-20 typing-cursor">
->>>>>>> 5fe9d7632eabe3ba7bc466817b48ce2a1c2cf056
             {displayedText}
           </p>
-
           <div className="flex flex-col sm:flex-row justify-between w-full mt-4 sm:mt-8 space-y-4 sm:space-y-0 sm:space-x-4">
-            <button
-<<<<<<< HEAD
-              className="bg-[#FFFFFF] hover:bg-orange-600 text-[#DF4E10] hover:text-[#FFFFFF] py-2 px-4 sm:py-3 sm:px-6 rounded-full cursor-pointer transition-all duration=500 text-sm sm:text-base"
-=======
-              className={`bg-[#FFFFFF] hover:bg-orange-600 text-[#DF4E10] hover:text-[#FFFFFF] py-[10px] px-[15px] sm:py-[12px] sm:px-[20px] rounded-full cursor-pointer transition-all duration=500 text-xs sm:text-sm`}
->>>>>>> 5fe9d7632eabe3ba7bc466817b48ce2a1c2cf056
-              onClick={() => alert("Book Appointment clicked")}
-            >
-              BOOK APPOINTMENT NOW
-            </button>
+            {/* Button that scrolls to target content */}
+            <BookmarkButton
+              onClick={() =>
+                document
+                  .getElementById("targetContent")
+                  ?.scrollIntoView({ behavior: "smooth" })
+              }
+            />
           </div>
         </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 5fe9d7632eabe3ba7bc466817b48ce2a1c2cf056
         <div>
           <img
             src={globe}
             alt="gif"
-<<<<<<< HEAD
-            className={`w-full h-full pr-20 ${
-              menuOpen ? "block" : "hidden"
-            } md:block`}
-=======
             className={`w-full h-full pr-20 hidden md:block mt-6`}
->>>>>>> 5fe9d7632eabe3ba7bc466817b48ce2a1c2cf056
           />
         </div>
       </div>
